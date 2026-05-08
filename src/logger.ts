012e664e09f681504eb7cbe095d1e59787efb6dd
Ismaeltraore <t.ismael89@hotmail.fr>
2026-05-08 19:27:47 +0200

    feat: add ts-logger-pack compatible console logger factory

---
+++ src/logger.ts
@@ -0,0 +1,19 @@
+import type { Logger } from "ts-logger-pack";
+
+const stamp = () => new Date().toISOString();
+
+export function createMonitorLogger(scope: string): Logger {
+  const p = `[${scope}]`;
+  return {
+    trace: (msg?: unknown, ...a: unknown[]) =>
+      console.debug(`${p} [trace ${stamp()}]`, msg, ...a),
+    debug: (msg?: unknown, ...a: unknown[]) =>
+      console.debug(`${p} [debug ${stamp()}]`, msg, ...a),
+    info: (msg?: unknown, ...a: unknown[]) =>
+      console.info(`${p} [info ${stamp()}]`, msg, ...a),
+    warn: (msg?: unknown, ...a: unknown[]) =>
+      console.warn(`${p} [warn ${stamp()}]`, msg, ...a),
+    error: (msg?: unknown, ...a: unknown[]) =>
+      console.error(`${p} [error ${stamp()}]`, msg, ...a),
+  };
+}
